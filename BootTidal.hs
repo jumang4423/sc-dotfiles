e5a9f8852e1adb17d20eac1de59ae61a415b30ba
jumang4423 <catontheskate4423@gmail.com>
2026-09-10 23:23:43 +0900

    yume samples, tmActiveDs query, today's sets

--- BootTidal.hs
+++ BootTidal.hs
@@ -4,6 +4,9 @@
 
 import Sound.Tidal.Boot
 import System.IO (hSetEncoding, stdout, utf8)
+import qualified Data.Map as TMMap
+import Control.Concurrent.MVar (readMVar)
+import Control.Monad (filterM)
 
 hSetEncoding stdout utf8
 
@@ -24,6 +27,19 @@ let fm = pF "fm"
     tinySmear = pF "tinySmear"
     disperser = pF "disperser"
     disperserFreq = pF "disperserFreq"
+
+-- text.management: print d-numbers holding sounding patterns (e.g. "TM-ACTIVE 1 3")
+-- (no standalone signature: ghci rejects a signature sent as its own line)
+tmActiveDs = do
+  pmap <- readMVar (sPMapMV tidal)
+  let solos = [n | (n, ps) <- TMMap.toList pmap, psSolo ps]
+  let cands = if null solos then map show ([1..16] :: [Int]) else solos
+  active <- filterM (tmActiveN pmap) cands
+  putStrLn ("TM-ACTIVE" ++ concatMap (" " ++) active)
+  where
+    tmActiveN pmap n = case TMMap.lookup n pmap of
+      Nothing -> return False
+      Just ps -> return (not (psMute ps) && not (null (queryArc (psPattern ps) (Arc 0 8))))
 
 :set prompt "tidal> "
 :set prompt-cont ""
